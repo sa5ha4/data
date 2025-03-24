606d16e2a83438b72ac06bac8b7cba7ab0873ffc
sa5ha4 <sa6ulja2008@gmail.com>
2025-03-24 15:31:18 +0200

    Update import requests.py

--- import requests.py
+++ import requests.py
@@ -1,10 +1,16 @@
+import matplotlib.pyplot as plt
 import requests
 
-response = requests.get("http://api.weatherapi.com/v1/forecast.json?key=a78417d484364aa29e674700252103&q=liepaja&days=1&aqi=yes&alerts=yes")
+response = requests.get("http://api.weatherapi.com/v1/forecast.json?key=a78417d484364aa29e674700252103&q=liepaja&days=4&aqi=yes&alerts=yes")
 
 data = response.json()
 
-for forecast in data["current"]["last_updated"]:
-    print(forecast)
+#for forecast in data["current"]["last_updated"]["time"]:
+#    print(forecast)
 
-print(data)
+print(data["forecast"]["forecastday"][0]["day"]["maxtemp_c"])
+
+fig, ax = plt.subplots()
+
+days = ['day 1', 'day 2', 'day 3', 'day 4']
+counts = []
